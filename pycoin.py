32e5e057fb177efc1ed6c3224ae3b215070487f0
Steve R <steven.richardspc@gmail.com>
2011-09-02 22:27:30 -0700

    outputs to string

--- pycoin.py
+++ pycoin.py
@@ -1,7 +1,11 @@
 #Bitcoin Arbitrage by Steven Richards <sbrichards@mit.edu>
 import pycurl
+import StringIO
 c = pycurl.Curl()
 c.setopt(pycurl.URL, "http://www.nyse-group.de/_external_rm/index_btc.php")
 c.setopt(pycurl.HTTPHEADER, ["Accept:"])
+result = StringIO.StringIO()
+c.setopt(pycurl.WRITEFUNCTION, result.write)
 c.perform()
-
+result = result.getvalue()
+print result
